7a44f87b6404a58b900f320fda0d851352341b10
Arti_Rhcp83 <aa.souza2006@gmail.com>
2024-11-27 22:09:37 -0300

    teste na branch

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 Este é apenas um teste para finalmente ver se o processo de envio remoto pc -> github está funcionando.
 :D
+
+Aparenta não haver mudanças bruscas ou casos de erro
--- codiguinho.c
+++ codiguinho.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 int main() {
-    int x, y,
+    int x, y;
     float resultado;
     scanf ("%d %d", &x, &y);
-    resultado = (x + x)/y + y;
-    printf ("%d\n", resultado)
+    resultado = (x + x) / y;
+    printf ("O resultado de (x + x)/y eh igual a %f\n", resultado);
 return 0;
 }
