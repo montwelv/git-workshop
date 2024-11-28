9b5f8b25bfc5957213da54923d88f32d7bd31d32
Arti_Rhcp83 <aa.souza2006@gmail.com>
2024-11-27 21:11:03 -0300

    mais um teste. Esta progredindo.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # git-workshop
+
+Este é apenas um teste para finalmente ver se o processo de envio remoto pc -> github está funcionando.
+:D
--- codiguinho.c
+++ codiguinho.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-
 int main() {
-    int x, y, resultado;
+    int x, y,
+    float resultado;
     scanf ("%d %d", &x, &y);
     resultado = (x + x)/y + y;
     printf ("%d\n", resultado)
